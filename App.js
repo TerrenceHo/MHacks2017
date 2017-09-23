8ac125dcf03bd7d09c6f282fa0387bd204fd5c0a
rutpatel <rutpatel010@gmail.com>
2017-09-23 02:24:34 -0400

    Main Page and environment setup

---
+++ App.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { StyleSheet, Animated, Text, View, Image } from 'react-native';
+
+class FadeInView extends React.Component {
+  state = {
+    fadeAnim: new Animated.Value(0),  // Initial value for opacity: 0
+  }
+
+  componentDidMount() {
+    Animated.timing(                  // Animate over time
+      this.state.fadeAnim,            // The animated value to drive
+      {
+        toValue: 1,                   // Animate to opacity: 1 (opaque)
+        duration: 2500,              // Make it take a while 2.5 seconds
+      }
+    ).start();                        // Starts the animation
+  }
+
+  render() {
+    let { fadeAnim } = this.state;
+
+    return (
+      <Animated.View                 // Special animatable View
+        style={{
+          ...this.props.style,
+          opacity: fadeAnim,         // Bind opacity to animated value
+        }}
+      >
+        {this.props.children}
+      </Animated.View>
+    );
+  }
+}
+
+export default class App extends React.Component {
+  
+  state = {
+    fadeAnim: new Animated.Value(0),  // Initial value for opacity: 0
+  }
+
+  componentDidMount() {
+    Animated.timing(                  // Animate over time
+      this.state.fadeAnim,            // The animated value to drive
+      {
+        toValue: 1,                   // Animate to opacity: 1 (opaque)
+        duration: 1000,              // Make it take a while
+      }
+    ).start();                        // Starts the animation
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image source={require('./assets/bg.jpg')} style={styles.backgroundImage} >
+           <FadeInView style={{top: '40%'}}>
+             <Text style={styles.text}>GroceryIQ</Text>
+          </FadeInView>
+        </Image>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+  },
+  text: {
+    textAlign: 'center',
+    color: 'white',
+    backgroundColor: 'rgba(0,0,0,0)',
+    fontSize: 52,
+    fontFamily: "Menlo-Italic",
+    fontWeight: 'bold'
+  },
+
+});
