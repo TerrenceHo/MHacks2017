86dc027f43be526f10d1de95618b5cc10a0ad428
Terrence Ho <terrenceho.books@gmail.com>
2017-09-24 02:12:36 -0400

    -Trying gcs

--- imgUpload.js
+++ imgUpload.js
@@ -23,11 +23,19 @@ ImgUpload.uploadToGcs = (req, res, next) => {
   // Can optionally add a path to the gcsname below by concatenating it before the filename
   const gcsname = req.file.originalname;
   console.log("gcsname", gcsname);
-  var file = bucket.file(gcsname);
-  file.makePublic(function(err, apiResponse){
-    console.error("ERROR:", err);
-    console.log("apiResponse", apiResponse);
-  });
+  gcs.bucket(bucketName).file(gcsname).makePublic()
+    .then(() => {
+      console.log("Made Public");
+    })
+    .catch((err) => {
+      console.error("ERROR:", err);
+    });
+
+  // var file = bucket.file(gcsname);
+  // file.makePublic(function(err, apiResponse){
+  //   console.error("ERROR:", err);
+  //   console.log("apiResponse", apiResponse);
+  // });
   // ).then(() => {
   //   console.log("Made Public");
   // })
